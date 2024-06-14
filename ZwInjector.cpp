08188fc4ffd46bf512adaa33281f0aaa9d2bbcb8
zwww2 <126341970+zwww2@users.noreply.github.com>
2024-06-14 22:19:30 +0300

    Add files via upload

---
+++ ZwInjector.cpp
@@ -0,0 +1,111 @@
+﻿#include <Windows.h>
+#include <winternl.h>
+#include <stdio.h>
+#include <iostream>
+
+BOOLEAN Log = TRUE;
+
+
+typedef NTSTATUS(NTAPI* NtOpenProcess__)(PHANDLE a, ACCESS_MASK b, OBJECT_ATTRIBUTES* c, CLIENT_ID* d);
+typedef NTSTATUS(NTAPI* NtAllocateVirtualMemory__)(HANDLE a, PVOID b, ULONG c, PULONG d, ULONG e, ULONG f);
+typedef NTSTATUS(NTAPI* NtWriteVirtualMemory__)(HANDLE a, PVOID b, PVOID c, ULONG d, PULONG e);
+typedef NTSTATUS(NTAPI* RtlCreateUserThread__)(HANDLE a, PSECURITY_DESCRIPTOR b, BOOLEAN c, ULONG d, PULONG e, PULONG f, PVOID g, PVOID h, PHANDLE i, CLIENT_ID* j);
+
+NtOpenProcess__ NtOpenProcess_;
+NtAllocateVirtualMemory__ NtAllocateVirtualMemory_;
+NtWriteVirtualMemory__ NtWriteVirtualMemory_;
+RtlCreateUserThread__ RtlCreateUserThread_;
+
+BOOLEAN InjectDll(HANDLE ProcessId, const char* path)
+{
+	HANDLE hProcess;
+	OBJECT_ATTRIBUTES oProcess;
+	CLIENT_ID cProcess;
+	InitializeObjectAttributes(&oProcess, 0, OBJ_INHERIT, 0, 0);
+	cProcess.UniqueProcess = ProcessId;
+	cProcess.UniqueThread = (HANDLE)0;
+
+	NTSTATUS status = NtOpenProcess_(&hProcess, PROCESS_ALL_ACCESS, &oProcess, &cProcess);
+
+	if (!NT_SUCCESS(status))
+	{
+		if (Log == TRUE)
+		{
+			printf("Failed open process 0x%X", status);
+		}
+		return FALSE;
+	}
+
+
+	PVOID data;
+	ULONG size = 8192;
+
+	status = NtAllocateVirtualMemory_(hProcess, &data, 0, &size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	
+	if (!NT_SUCCESS(status))
+	{
+
+		CloseHandle(hProcess);
+		if (Log == TRUE)
+		{
+			printf("Failed allocate process memory 0x%X", status);
+		}
+		return FALSE;
+	}
+
+	
+	status = NtWriteVirtualMemory_(hProcess, data, (LPSTR)path, strlen(path) + 1, 0);
+
+	if (!NT_SUCCESS(status))
+	{
+		CloseHandle(hProcess);
+		if (Log == TRUE)
+		{
+			printf("Failed write process memory 0x%X", status);
+		}
+		return FALSE;
+	}
+
+	HANDLE hThread;
+	CLIENT_ID cThread;
+
+	status = RtlCreateUserThread_(hProcess, 0, FALSE, 0, 0, 0, (PVOID)LoadLibraryA, data, &hThread, &cThread);
+
+	if (!NT_SUCCESS(status))
+	{
+		CloseHandle(hProcess);
+		if (Log == TRUE)
+		{
+			
+
+			printf("Failed create thread 0x%X", status);
+		}
+		return FALSE;
+	}
+
+	CloseHandle(hProcess);
+	CloseHandle(hThread);
+
+
+	return TRUE;
+
+}
+
+
+
+
+int main()
+{
+	NtOpenProcess_ = (NtOpenProcess__)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtOpenProcess");
+	NtAllocateVirtualMemory_ = (NtAllocateVirtualMemory__)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtAllocateVirtualMemory");
+	NtWriteVirtualMemory_ = (NtWriteVirtualMemory__)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtWriteVirtualMemory");
+	RtlCreateUserThread_ = (RtlCreateUserThread__)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlCreateUserThread");
+	
+	
+	InjectDll((HANDLE)8868, "D:\\Games\\12\\ZwHook\\x64\\Release\\ZwHook.dll");
+	
+
+	Sleep(-1);
+}
+
+
